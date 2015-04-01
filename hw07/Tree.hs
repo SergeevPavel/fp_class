da63eeba6f72f49960cc36768ca81eab65d1477a
valis <valery.isaev@gmail.com>
2015-04-01 21:02:31 +0300

    prettify

--- hw07/Tree.hs
+++ hw07/Tree.hs
@@ -8,10 +8,8 @@ data Tree a = Node { value :: a, children :: [Tree a] } deriving Eq
 
 -- show и read должны работать как описано в тестах в Main.hs
 instance Show a => Show (Tree a) where
-    show (Node x chld) = show x ++ childrenStr
-        where
-            childrenStr = case chld of []    -> ""
-                                       chlds -> ":{" ++ (L.intercalate "," (map show chlds))  ++ "}"
+    show (Node x []) = show x
+    show (Node x chlds) = show x ++ ":{" ++ (L.intercalate "," (map show chlds))  ++ "}"
 
 instance Read a => Read (Tree a) where
     readsPrec _ str = undefined 
