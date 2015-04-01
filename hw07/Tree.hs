edb4eaeecc9c3072553bf520d797dc855cf4eba5
pavel <sergeev.pavel.al@gmail.com>
2015-04-01 20:49:51 +0300

    tree implementation

--- hw07/Tree.hs
+++ hw07/Tree.hs
@@ -1,15 +1,20 @@
 module Tree where
 
+import qualified Data.List as L
+
 -- (2 балла)
 
 data Tree a = Node { value :: a, children :: [Tree a] } deriving Eq
 
 -- show и read должны работать как описано в тестах в Main.hs
 instance Show a => Show (Tree a) where
-    show = undefined
+    show (Node x chld) = show x ++ childrenStr
+        where
+            childrenStr = case chld of []    -> ""
+                                       chlds -> ":{" ++ (L.intercalate "," (map show chlds))  ++ "}"
 
 instance Read a => Read (Tree a) where
-    readsPrec = undefined
+    readsPrec _ str = undefined 
 
 instance Functor Tree where
-    fmap = undefined
+    fmap fun (Node a chld) = Node (fun a) (map (fmap fun) chld) 
