5733cab27ea46fceadcf2ca4e455ce7f453c089a
pavel <sergeev.pavel.al@gmail.com>
2015-04-12 03:07:38 +0300

    cat implemented

--- hw09/cat.hs
+++ hw09/cat.hs
@@ -1,5 +1,7 @@
-import Control.Exception(catch)
-
+import Control.Exception(catch, IOException)
+import System.Environment
+import System.IO
+import Control.Monad
 {-
 cat принимает имена файлов и выводит их содержимое на экран.
 Если в cat не передаются параметры, то она копирует stdin в stdout.
@@ -7,5 +9,19 @@ cat принимает имена файлов и выводит их содер
 (1.5 балла)
 -}
 
+
+printFile :: String -> IO ()
+printFile fileName = do
+    content <- catch (readFile fileName)
+        (\e -> do let err = show (e :: IOException)
+                  hPutStr stderr ("Warning: Couldn't open " ++ fileName ++ ": " ++ err ++ "\n")
+                  return "")
+    putStr content
+
+
 main :: IO ()
-main = undefined
+main = do
+    fileNames <- getArgs
+    if (length fileNames == 0) then forever (getLine >>= putStrLn)
+                               else forM_ fileNames printFile
+    
