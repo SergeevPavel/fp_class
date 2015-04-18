c58006747e58c3d60085fe9b9533f3198b55906b
isaev <valery.isaev@gmail.com>
2015-04-18 17:20:18 +0300

    Add commentary

--- hw09/guess.hs
+++ hw09/guess.hs
@@ -14,15 +14,15 @@ import System.Exit
 runTurn :: Integer -> Integer -> IO ()
 runTurn x _ = do
     putStr "Try guess the number:"
-    y <- readLn :: IO Integer
-    case signum (x - y) of -1 -> putStrLn "less"
-                           1 -> putStrLn "greater"
-                           0 -> putStrLn "you right" >> exitSuccess
-    return ()
+    -- y <- readLn :: IO Integer -- зачем аннотация типа? Он сам может его вывести.
+    y <- readLn
+    case compare x y of LT -> putStrLn "less"
+                        GT -> putStrLn "greater"
+                        EQ -> putStrLn "you right" >> exitSuccess
 
 main :: IO ()
 main = do
-    x <- randomRIO (1 :: Integer, 100 :: Integer)
+    x <- randomRIO (1, 100)
     forM_ [1..5] $ runTurn x
     putStrLn "you lose"
-    return ()
+    -- return () -- зачем это здесь? return ничего не делает. Его нужно взывать только если нужно вернуть что-то отличное от того, что возвращает последняя строчка в do-списке.
