b19b6144698a2f8646706d8f2fd682d8ab679980
pavel <sergeev.pavel.al@gmail.com>
2015-04-21 02:36:51 +0300

    Behaviour implemented

--- hw09/Behaviour.hs
+++ hw09/Behaviour.hs
@@ -2,9 +2,13 @@
 Реализуйте runBehaviour
 (1.5 балла)
 -}
+import Control.Monad
+import System.IO
+import Data.List
+import System.IO.Unsafe
 
-data Request  = Get           | Put String
-data Response = Result String | OK
+data Request  = Get           | Put String deriving Show
+data Response = Result String | OK deriving Show
 
 type Behaviour = [Response] -> [Request]
 
@@ -14,8 +18,18 @@ prog ~(OK : x : xs) = Put "more? " : Get : case x of
     Result "yes" -> prog xs
     _ -> Put "yes or no!" : prog (tail xs)
 
-runBehaviour :: Behaviour -> IO ()
-runBehaviour = undefined
+executeRequest :: Request -> IO Response
+executeRequest Get = do
+    input <- getLine 
+    return $ Result input
 
-main :: IO ()
+executeRequest (Put output) = do
+    putStrLn output
+    return OK
+
+runBehaviour :: Behaviour -> IO ()
+runBehaviour behaviour = do
+    let responses = map (unsafePerformIO . executeRequest) . behaviour $ responses
+    foldr seq (return ()) responses
+
 main = runBehaviour prog
